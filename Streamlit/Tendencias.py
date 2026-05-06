b64ef9c1993ddf0073c0e6e34283d8b8d9cf6c48
carlosazmora <carloszulmor@unisabana.edu.co>
2026-05-06 16:17:06 -0500

    Bloqueo del uso de Claude
    Dado que hemos publicado la página, bloqueamos la generación de Tendencias para evitar sobrecostos.
    
    Hemos indicado a los usuarios que nos contacten por LinkedIn si les interesa explorarlo

--- Streamlit/Tendencias.py
+++ Streamlit/Tendencias.py
@@ -9,8 +9,10 @@ def mostrar_tendencias_e_insights():
     if "Sin datos" in contexto:
         st.info("⚠️ Sin datos cargados. Ve al **Panel de Actualización** y presiona '🔄 Actualizar DB' para habilitarlo.")
  
-    if st.button("Generar Análisis con Claude", disabled=("Sin datos" in contexto)):
+    # if st.button("Generar Análisis con Claude", disabled=("Sin datos" in contexto)):
+    if st.button("Generar Análisis con Claude", disabled = True):
         generar_insight_claude(
             panel_nombre="Tendencias e Insights Estratégicos",
             contexto_datos=contexto
         )
+    st.caption("🔍 *Nota: La función de análisis con Claude está temporalmente deshabilitada. Si te interesa explorarla, contáctanos en www.linkedin.com/in/carlosazmora, https://www.linkedin.com/in/celyjorge/, https://www.linkedin.com/in/alejandro-morantes-castellanos, https://www.linkedin.com/in/dianapaolaquintero?utm_source=share_via&utm_content=profile&utm_medium=member_android*")
